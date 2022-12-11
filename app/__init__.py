ada462302c6e1cbc3a49e2888da84fd5778ca9fd
dusean435 <dusean435@gmail.com>
2022-12-12 00:25:47 +0300

    Add static routes for index and 404

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 
 def create_app(config):
-    app=Flask(__name__)
+    app=Flask(__name__, static_folder='../static')
     app.config.from_object(config)
 
     CORS(app)
+
     @app.route("/")
     def index():
-      return "<h1>*************IT WORKS*************</h1>"
+        return app.send_static_file('index.html')
 
+    @app.errorhandler(404)
+    def not_found(err):
+        return app.send_static_file('404.html')
+    
 
     return app
-    
